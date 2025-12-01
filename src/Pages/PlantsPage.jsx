eb86fc034304dffbe5462d42d83cda8e7e2398a4
Md-Sojib-Khan <007shojibkhan@gmail.com>
2025-12-01 23:03:49 +0600

    add sort and filter methode in all plants page

--- src/Pages/PlantsPage.jsx
+++ src/Pages/PlantsPage.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router';
 import PlantCard from '../Components/PlantCard';
 
 const PlantsPage = () => {
     const data = useLoaderData()
+    const [search, setSearch] = useState("");
+    const [sort, setSort] = useState("all");
+
+    const filteredPlants = data
+        .filter(plant =>
+            plant.plantName.toLowerCase().includes(search.toLowerCase())
+        )
+        .sort((a, b) => {
+            if (sort === "high") return b.rating - a.rating;
+            if (sort === "low") return a.rating - b.rating;
+            return 0;
+        });
     return (
         <div className='py-10 bg-gradient-to-br from-green-50 to-emerald-100'>
             <div>
@@ -14,9 +26,27 @@ const PlantsPage = () => {
                     From air-purifying greens to vibrant succulents, find the perfect plant that matches your lifestyle. Let nature grow closer to you — one leaf at a time.
                 </p>
             </div>
+            <div className="flex justify-between gap-4 mb-8 flex-wrap w-11/12 mx-auto">
+                <input
+                    type="text"
+                    placeholder="Search plants..."
+                    className="p-3 border rounded-lg min-w-[200px]"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+                <select
+                    className="p-3 border rounded-lg"
+                    value={sort}
+                    onChange={(e) => setSort(e.target.value)}
+                >
+                    <option value="all">Sort By Ratings</option>
+                    <option value="high">High to Low</option>
+                    <option value="low">Low to High</option>
+                </select>
+            </div>
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 w-11/12 mx-auto'>
                 {
-                    data.map(PlantData => <PlantCard PlantData={PlantData} key={PlantData.plantId}></PlantCard>)
+                    filteredPlants.map(PlantData => <PlantCard PlantData={PlantData} key={PlantData.plantId}></PlantCard>)
                 }
             </div>
         </div>
